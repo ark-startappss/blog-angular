3176f78032719bf79564287331bce5054229054f
ark-startappss <aadarsh.kawatra@startappss.com>
2025-03-06 14:43:21 +0530

    dashboard UI done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 
 // local imports
 import { LoginComponent } from './login/login.component';
-import { BlogsComponent } from './blogs/blogs.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'blogs', component: BlogsComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterModule, MatButtonModule, MatCardModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  blogs = [
+    {
+      id: 1,
+      title: 'Blog 1',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+    {
+      id: 2,
+      title: 'Blog 2',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+    {
+      id: 3,
+      title: 'Blog 3',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+    {
+      id: 4,
+      title: 'Blog 4',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+    {
+      id: 5,
+      title: 'Blog 5',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+    {
+      id: 6,
+      title: 'Blog 6',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+    {
+      id: 7,
+      title: 'Blog 7',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla ratione dolorem eos facilis id fugiat ipsam facere nam veritatis.',
+    },
+  ];
+}
